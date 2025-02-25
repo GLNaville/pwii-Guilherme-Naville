4e82f370fce907ca191ed4fc05d9e68b4ba6b7cb
Guilherme Lima <gui.lima.naville@gmail.com>
2025-02-25 16:34:52 -0300

    Aula de PHP com exemplos de concatenação de variáveis

---
+++ ola.php
@@ -0,0 +1,10 @@
+
+<?php
+echo "Olá Mundo!<br><br>";
+$nome = "Guilherme";
+$numero = 4;
+$sobrenome = "Lima Naville";
+
+echo "${nome} <h1> ${sobrenome} </h1>";
+echo $numero . "<p>" . $nome . " " . $sobrenome . "<p>";
+?>
